17f9b76250858518fdfe1a1d452fdad102c476b7
priya-doshi <47188325+priya-doshi@users.noreply.github.com>
2021-02-17 16:34:44 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,26 @@
-sudo apt-get install -y nodejs gnupg
-
-
-wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-
-
-sudo apt-get update
-
-
-sudo apt-get install -y mongodb-org
-
-cd backEnd
-
-npm install
-
-npm install mongoose express dotenv nodemon cors
-
-cd ..
-
-cd frontEnd
-
-npm install --save bootstrap
-
-npm install axios
+# Any installation related commands
+ 
+ # sudo apt-get install -y abc
+ 
+ # add nodejs 12 ppa (personal package archive) from nodesource
+ curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+ # install nodejs and npm
+ sudo apt-get install -y nodejs
+ # import mongodb 4.0 public gpg key
+ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+ 
+ # create the /etc/apt/sources.list.d/mongodb-org-4.0.list file for mongodb
+ echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+ 
+ # reload local package database
+ sudo apt-get update
+ 
+ # install the latest version of mongodb
+ sudo apt-get install -y mongodb-org
+ 
+  # set mongodb to start automatically on system startup
+ sudo systemctl enable mongod
+ 
+ 
+ # start mongodb
+ sudo systemctl start mongod
